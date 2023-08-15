e10af6aeafbaea55c5c10a91011b877bff75bd49
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-08-15 19:15:42 +0100

    added asset extration engine

--- engine.py
+++ engine.py
@@ -111,18 +111,20 @@ class BaseAssetExtractionEngine(metaclass=ABCMeta):
 
     def run(self):
         asyncio.run(self._run())
-        # logger.info(f"Running for transaction with hash -> {self.data['hash']}")
-        # token_id = self.token_id_extractor.extract()
-        # if token_id is None:
-        #     logger.info(f"No Token ID For Transaction with hash: {self.data['hash']}")
-        #     return
-        # token_uri = None
-        # if "URI" not in self.data:
-        #     token_uri = DomainURIExtractor.extract(self.data, token_id)
-        # else:
-        #     token_uri = self.token_uri_extractor.extract()
-        # await self._extract_assets(token_id, token_uri)
 
+class AssetExtractionEngine(BaseAssetExtractionEngine):
+    async def _run(self):
+        logger.info(f"Running for transaction with hash -> {self.data['hash']}")
+        token_id = self.token_id_extractor.extract()
+        if token_id is None:
+            logger.info(f"No Token ID For Transaction with hash: {self.data['hash']}")
+            return
+        token_uri = None
+        if "URI" not in self.data:
+            token_uri = DomainURIExtractor.extract(self.data, token_id)
+        else:
+            token_uri = self.token_uri_extractor.extract()
+        await self._extract_assets(token_id, token_uri)
 
 class RetryEngine(BaseAssetExtractionEngine):
     def __init__(self, data=None, path=None):
@@ -180,165 +182,21 @@ class PublicRetryEngine(BaseAssetExtractionEngine):
             await self.writer.write_json(f"publicapinotfound/error/{self.token_id}.json", error)
 
 
-class Engine:
-    def __init__(self, data):
-        self.data = data
-        self.writer = AsyncS3FileWriter()
-        self.fetcher = Fetcher()
-        self.token_id_extractor = TokenIDExtractor(data)
-        self.token_uri_extractor = TokenURIExtractor(data)
-
-    async def _dump_image(self, content: bytes, token_id: str, content_type):
-        full, resized = process_image(content)
-        ext = content_type.split("/")[1]
-        await self.writer.write_image(f"assets/images/{token_id}/200px/image", resized, "image/png")
-        await self.writer.write_image(f"assets/images/{token_id}/200px/image.png", resized, "image/png")
-        await self.writer.write_image(f"assets/images/{token_id}/full/image", full, content_type)
-        await self.writer.write_image(f"assets/images/{token_id}/full/image.{ext}", full, content_type)
-
-    async def _dump_metadata(self, data, token_id):
-        await self.writer.write_json(f"assets/metadata/{token_id}/metadata", data)
-        await self.writer.write_json(f"assets/metadata/{token_id}/metadata.json", data)
-
-    async def _dump_file(self, file_type, token_id, content, content_type, extension):
-        await self.writer.write_media(f"assets/{file_type}s/{token_id}/{file_type}", content, content_type)
-        await self.writer.write_media(f"assets/{file_type}s/{token_id}/{file_type}.{extension}", content, content_type)
-
-    async def retry_text_metadata(self, meta_data, token_id):
-        content_exists = meta_data.get("content")  # noqa
-        if content_exists:
-            content, content_type = await self.fetcher.fetch(content_exists.replace("cid:", ""))
-            file_type = content_type.split("/")[0]
-            if file_type == "image":
-                await self._dump_image(content, token_id, content_type)
-            else:
-                ext = content_type.split("/")[1]
-                await self._dump_file(file_type, token_id, content, content_type, ext)
-        await self._dump_metadata(meta_data, token_id)
-
-        # Search for Other Assets in the MetaData Json and Upload to s3
-        image_url = meta_data.get("image", meta_data.get("image_url"))  # noqa
-        video_url = meta_data.get("video", meta_data.get("video_url"))
-        file_url = meta_data.get("file", meta_data.get("file_url"))
-        animation_url = meta_data.get("animation", meta_data.get("animation_url"))
-        audio_url = meta_data.get("audio", meta_data.get("audio_url"))
-        thumbnail_url = meta_data.get("thumbnail", meta_data.get("thumbnail_url"))
-        if image_url:
-            image_content, content_type = await self.fetcher.fetch(image_url)
-            if image_content is not None:
-                await self._dump_image(image_content, token_id, content_type)
-        if video_url:  # noqa
-            video_content, content_type = await self.fetcher.fetch(video_url)
-            if video_content:
-                ext = content_type.split("/")[1]
-                await self._dump_file("video", token_id, video_content, content_type, ext)
-        if file_url:  # noqa
-            logger.info(f"Found File URL: {file_url}")
-        if audio_url:
-            logger.info(f"Found Audio URL: {audio_url}")
-        if thumbnail_url:
-            logger.info(f"Found Thumbnail URL: {thumbnail_url}")
-        if animation_url:  # noqa
-            animation_content, content_type = await self.fetcher.fetch(animation_url)
-            ext = content_type.split("/")[1]
-            if animation_content:
-                await self._dump_file("animation", token_id, animation_content, content_type, ext)
-
-    async def _extract_assets(self, token_id, token_uri):
-        if token_uri is None:
-            logger.info(f"No Token URI Found For Object With ID: {self.data['ledger_index']}")
-            return
-        content, content_type = await self.fetcher.fetch(token_uri)
-        if content:
-            meta_data = {}
-            file_type = content_type.split("/")[0]
-            if file_type == "application" or file_type == "text":
-                meta_data = json.loads(content)
-                content_exists = meta_data.get("content")
-                if content_exists:
-                    content, content_type = await self.fetcher.fetch(content_exists.replace("cid:", ""))
-                    file_type = content_type.split("/")[0]
-                    if file_type == "image":
-                        await self._dump_image(content, token_id, content_type)
-                    else:
-                        ext = content_type.split("/")[1]
-                        await self._dump_file(file_type, token_id, content, content_type, ext)
-                await self._dump_metadata(meta_data, token_id)
-            elif file_type == "image":
-                meta_data = {
-                    "image": token_uri
-                }
-                await self._dump_metadata(meta_data, token_id)
-                logger.info("Processing possible image metadata")
-                await self._dump_image(content, token_id, content_type)
-                return
-            else:
-                logger.info(f"Got FileType {file_type} For Metadata")
-                meta_data = {
-                    file_type: token_uri
-                }
-                ext = content_type.split("/")[1]
-                await self._dump_metadata(meta_data, token_id)
-                await self._dump_file(file_type, token_id, content, content_type, ext)
-
-            # Search for Other Assets in the MetaData Json and Upload to s3
-            image_url = meta_data.get("image", meta_data.get("image_url"))
-            video_url = meta_data.get("video", meta_data.get("video_url"))
-            file_url = meta_data.get("file", meta_data.get("file_url"))
-            animation_url = meta_data.get("animation", meta_data.get("animation_url"))
-            audio_url = meta_data.get("audio", meta_data.get("audio_url"))
-            thumbnail_url = meta_data.get("thumbnail", meta_data.get("thumbnail_url"))
-            if image_url:
-                image_content, content_type = await self.fetcher.fetch(image_url)
-                if image_content is not None:
-                    await self._dump_image(image_content, token_id, content_type)
-            if video_url:  # noqa
-                video_content, content_type = await self.fetcher.fetch(video_url)
-                ext = content_type.split("/")[1]
-                if video_content:
-                    await self._dump_file("video", token_id, video_content, content_type, ext)
-            if file_url:
-                logger.info(f"Found File URL: {file_url}")
-            if audio_url:
-                logger.info(f"Found Audio URL: {audio_url}")
-            if thumbnail_url:
-                logger.info(f"Found Thumbnail URL: {thumbnail_url}")
-            if animation_url:  # noqa
-                animation_content, content_type = await self.fetcher.fetch(animation_url)
-                ext = content_type.split("/")[1]
-                if animation_content:
-                    await self._dump_file("animation", token_id, animation_content, content_type, ext)
-            logger.info(f"Completed dump for Token ID -> {token_id}\n")
-        else:
-            raise NoMetaDataException(f"Could Not Fetch Metadata for {token_uri}")
-
-    async def run(self):
-        logger.info(f"Running for transaction with hash -> {self.data['hash']}")
-        token_id = self.token_id_extractor.extract()
-        if token_id is None:
-            logger.info(f"No Token ID For Transaction with hash: {self.data['hash']}")
-            return
-        token_uri = None
-        if "URI" not in self.data:
-            token_uri = DomainURIExtractor.extract(self.data, token_id)
-        else:
-            token_uri = self.token_uri_extractor.extract()
-        await self._extract_assets(token_id, token_uri)
-
-    async def retry_v2(self, token_id, token_uri, issuer, completed, errors):
-        logger.info(f"starting retry for {token_id}")
-        if type(token_uri) == float or token_uri is None:
-            data = {"Issuer": issuer}
-            token_uri = await DomainURIExtractor.async_extract(data, token_id)
-        else:
-            try:
-                token_uri = TokenURIExtractor({"URI": token_uri}).extract()
-            except ValueError:
-                token_uri = token_uri
-        try:
-            await self._extract_assets(token_id, token_uri)
-        except Exception as e:
-            logger.error(e)
-            errors.append({"token_id": token_id, "issuer": issuer, "uri": token_uri, "error": str(e)})
-        completed[token_id] = True
-        logger.error(f"completed retry for {token_id}")
+# class NFTProjectsRetry:
+#     async def retry_v2(self, token_id, token_uri, issuer, completed, errors):
+#         logger.info(f"starting retry for {token_id}")
+#         if type(token_uri) == float or token_uri is None:
+#             data = {"Issuer": issuer}
+#             token_uri = await DomainURIExtractor.async_extract(data, token_id)
+#         else:
+#             try:
+#                 token_uri = TokenURIExtractor({"URI": token_uri}).extract()
+#             except ValueError:
+#                 token_uri = token_uri
+#         try:
+#             await self._extract_assets(token_id, token_uri)
+#         except Exception as e:
+#             logger.error(e)
+#             errors.append({"token_id": token_id, "issuer": issuer, "uri": token_uri, "error": str(e)})
+#         completed[token_id] = True
+#         logger.error(f"completed retry for {token_id}")
